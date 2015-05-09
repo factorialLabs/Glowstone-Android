2b0d474bc32c05362517c8e0547354f4a98201ba
Yu Chen Hou <houyuchen66@gmail.com>
2015-05-09 02:08:25 -0400

    task barebones

--- mobile/src/main/java/factoriallabs/com/baconbeacon/tasks/QueryTableStatusTask.java
+++ mobile/src/main/java/factoriallabs/com/baconbeacon/tasks/QueryTableStatusTask.java
@@ -3,5 +3,11 @@ package factoriallabs.com.baconbeacon.tasks;
 /**
  * Created by yuchen.hou on 15-05-09.
  */
-public class QueryTableStatusTask {
+public class QueryTableStatusTask extends Task{
+
+    QueryTableStatusTask(OnResultListener l){
+        super("table",l);
+    }
+
+
 }
--- mobile/src/main/java/factoriallabs/com/baconbeacon/tasks/Task.java
+++ mobile/src/main/java/factoriallabs/com/baconbeacon/tasks/Task.java
@@ -1,7 +1,38 @@
 package factoriallabs.com.baconbeacon.tasks;
 
+import com.parse.GetCallback;
+import com.parse.ParseException;
+import com.parse.ParseObject;
+import com.parse.ParseQuery;
+
 /**
  * Created by yuchen.hou on 15-05-09.
  */
 public class Task {
+
+    public interface OnResultListener{
+        void onDone(ParseObject object);
+    }
+
+    private OnResultListener callback;
+    ParseQuery<ParseObject> query;
+
+    Task(String endpoint, OnResultListener l){
+        query = ParseQuery.getQuery(endpoint);
+        callback = l;
+    }
+
+    public void run(){
+        query.getInBackground("", new GetCallback<ParseObject>() {
+            public void done(ParseObject object, ParseException e) {
+                if (e == null) {
+                    //call back
+                    callback.onDone(object);
+                } else {
+                    // something went wrong
+                    callback.onDone(null);
+                }
+            }
+        });
+    }
 }
