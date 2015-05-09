72e0a8d0cc87690faf843a3573409041537bca60
Kong-Artist <stevendjkong@gmail.com>
2015-05-09 03:05:30 -0400

    Added a table picture with two buttons for table activity

--- mobile/src/main/java/factoriallabs/com/baconbeacon/TableActivity.java
+++ mobile/src/main/java/factoriallabs/com/baconbeacon/TableActivity.java
@@ -1,18 +1,59 @@
 package factoriallabs.com.baconbeacon;
 
 import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
 import android.support.v7.app.AppCompatActivity;
+<<<<<<< Updated upstream
 import android.view.Menu;
 import android.view.MenuItem;
+=======
+import android.support.v7.app.ActionBarActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.Window;
+import android.view.WindowManager;
+import android.app.Fragment;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
+import android.widget.FrameLayout;
+import android.view.ViewGroup.LayoutParams;
 
 /**
  * Created by yuchen.hou on 15-05-08.
  */
-public class TableActivity extends AppCompatActivity {
+
+
+import factoriallabs.com.baconbeacon.R;
+import factoriallabs.com.baconbeacon.fragments.MenuFragment;
+import factoriallabs.com.baconbeacon.fragments.TableFragment;
+
+public class TableActivity extends FragmentActivity {
+
+    private static final int CONTENT_VIEW_ID = 10101010;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.fragment_menu);
+        //this.requessWindowFeature(Window.FEATURE_NO_TITLE);
+        //this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_table);
+
+        FrameLayout frame = (FrameLayout) findViewById(R.id.container);
+
+        if (savedInstanceState == null) {
+            /* During initial setup, plug in the details fragment.
+            TableFragment table = new TableFragment();
+            FragmentTransaction ft = getFragmentManager().beginTransaction();
+            ft.add(CONTENT_VIEW_ID, table).commit();
+            table.setArguments(getIntent().getExtras());
+            getFragmentManager().beginTransaction().add(android.R.id.content, details).commit(); */
+        }
+
+        FragmentManager manager = getFragmentManager();
+        FragmentTransaction transaction = manager.beginTransaction();
+
+        transaction.replace(R.id.container, TableFragment.newInstance(null,null),"tablefragment"); // newInstance() is a static factory method.
+        transaction.commit();
     }
 
 
