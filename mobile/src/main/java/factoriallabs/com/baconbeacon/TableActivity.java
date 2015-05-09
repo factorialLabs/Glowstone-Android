19f24efc25df2fe86cd355b052222929d6128df8
Yu Chen Hou <houyuchen66@gmail.com>
2015-05-09 00:08:11 -0400

    barebone classes

---
+++ mobile/src/main/java/factoriallabs/com/baconbeacon/TableActivity.java
@@ -0,0 +1,13 @@
+package factoriallabs.com.baconbeacon;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import it.neokree.materialnavigationdrawer.MaterialNavigationDrawer;
+
+/**
+ * Created by yuchen.hou on 15-05-08.
+ */
+public class TableActivity extends AppCompatActivity {
+
+}
